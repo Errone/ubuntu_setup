4c4f5c457da94f7d3e61e2457745ef01e0f14f8f
uu <>
2025-12-05 14:48:35 +0800

    change source

--- ranger_setup.sh
+++ ranger_setup.sh
@@ -2,4 +2,5 @@
 
 # clone the nvim git repository
 mkdir -p ~/.config && cd ~/.config
-git clone https://github.com/minrui-hust/ranger.git --recursive
+git clone https://github.com/Errone/ranger.git --recursive
+
--- tmux_setup.sh
+++ tmux_setup.sh
@@ -1,6 +1,5 @@
 # clone the nvim git repository
 mkdir -p ~/.config && cd ~/.config
-git clone https://github.com/minrui-hust/tmux.git
-
+git clone https://github.com/Errone/tmux.git
 # install
 bash ~/.config/tmux/install.bash
